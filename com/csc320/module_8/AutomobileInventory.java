6e25aa7f7256a9756caa2a32c8802639237d3a09
CSUG-Cole <146023803+CSUG-Cole@users.noreply.github.com>
2024-04-02 16:55:40 -0700

    Add AutomobileInventory

---
+++ com/csc320/module_8/AutomobileInventory.java
@@ -0,0 +1,13 @@
+package com.csc320.module_8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutomobileInventory {
+    /* This list will hold the inventory of the program */
+    private final List<Automobile> inventory = new ArrayList<Automobile>();
+
+    public static void main(String[] args) {
+
+    }
+}

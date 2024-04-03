c37101d19c0046decd5f6d5a3599e6aa29858834
CSUG-Cole <146023803+CSUG-Cole@users.noreply.github.com>
2024-04-02 17:10:19 -0700

    Define addAutomobile class & initialize inventory

--- com/csc320/module_8/AutomobileInventory.java
+++ com/csc320/module_8/AutomobileInventory.java
@@ -5,9 +5,26 @@ import java.util.List;
 
 public class AutomobileInventory {
     /* This list will hold the inventory of the program */
-    private final List<Automobile> inventory = new ArrayList<Automobile>();
+    private static final List<Automobile> inventory = new ArrayList<Automobile>();
 
     public static void main(String[] args) {
+        /* Fill the inventory with some Automobiles */
+        Automobile defaultAuto = new Automobile();
+        Automobile customAuto = new Automobile("Toyota", "Tacoma", 1999, "Silver", 80_000);
+        if (addAutomobile(defaultAuto) || addAutomobile(customAuto)) {
+            System.out.println("ERROR: Failed to initialize AutomobileInventory. Exiting.");
+        }
+    }
 
+    private static boolean addAutomobile(Automobile auto) {
+        boolean result = inventory.add(new Automobile(auto));
+        if (result) {
+            System.out.print("Successfully added automobile to inventory.");
+            auto.printDetails();
+        } else {
+            System.out.println("Failed to add automobile to inventory.");
+            auto.printDetails();
+        }
+        return result;
     }
 }

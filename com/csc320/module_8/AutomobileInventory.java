d9836702aa0c76437d4ef355bfd5feffd3fc99c6
CSUG-Cole <146023803+CSUG-Cole@users.noreply.github.com>
2024-04-02 17:27:49 -0700

    Add comments and error handling

--- com/csc320/module_8/AutomobileInventory.java
+++ com/csc320/module_8/AutomobileInventory.java
@@ -8,16 +8,30 @@ public class AutomobileInventory {
     private static final List<Automobile> inventory = new ArrayList<Automobile>();
 
     public static void main(String[] args) {
-        /* Fill the inventory with some Automobiles */
+        /* Create some default automobiles */
         Automobile defaultAuto = new Automobile();
         Automobile customAuto = new Automobile("Toyota", "Tacoma", 1999, "Silver", 80_000);
+
+        /* Fill the inventor with default automobiles */
         if (addAutomobile(defaultAuto) || addAutomobile(customAuto)) {
             System.out.println("ERROR: Failed to initialize AutomobileInventory. Exiting.");
+            System.exit(-1); /* -1 to indicate an error with the program */
+        }
+
+        try {
+            runInventoryLoop();
+        } catch (IllegalArgumentException e) {
+            System.out.println("ERROR: Program encountered a critical error: " + e.getMessage());
+            System.out.println("Exiting.");
+            System.exit(-1); /* -1 to indicate an error with the program */
         }
     }
 
+    /* Method to add an automobile to the class inventory */
     private static boolean addAutomobile(Automobile auto) {
+        /* Store the result to return later and check for errors */
         boolean result = inventory.add(new Automobile(auto));
+
         if (result) {
             System.out.print("Successfully added automobile to inventory.");
             auto.printDetails();
